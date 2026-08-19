72695fa7650e60e32da551c8dfbdbd813ae3835c
anshikaa14s <anshikasingh5428@gmail.com>
2026-08-19 21:15:35 +0530

    Complete Day 2 Kafka video streaming pipeline

---
+++ consumer/video_consumer.py
@@ -0,0 +1,43 @@
+from kafka import KafkaConsumer
+from pathlib import Path
+import json
+
+KAFKA_SERVER = "localhost:9092"
+TOPIC = "video_chunks"
+
+OUTPUT_DIR = Path("data/received_chunks")
+OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
+
+
+consumer = KafkaConsumer(
+    TOPIC,
+    bootstrap_servers=KAFKA_SERVER,
+    auto_offset_reset="earliest",
+    enable_auto_commit=True,
+    value_deserializer=lambda v: json.loads(v.decode("utf-8"))
+)
+
+
+print("Waiting for video chunks...")
+
+
+for message in consumer:
+
+    data = message.value
+
+    video_id = data["video_id"]
+    chunk_id = data["chunk_id"]
+    filename = data["filename"]
+
+    chunk_data = bytes.fromhex(data["data"])
+
+    output_path = OUTPUT_DIR / f"received_{filename}"
+
+    with open(output_path, "wb") as f:
+        f.write(chunk_data)
+
+    print(
+        f"Received: {filename} | "
+        f"Video: {video_id} | "
+        f"Chunk ID: {chunk_id}"
+    )
---
+++ producer/video_producer.py
@@ -0,0 +1,38 @@
+from kafka import KafkaProducer
+from pathlib import Path
+import json
+
+KAFKA_SERVER = "localhost:9092"
+TOPIC = "video_chunks"
+
+CHUNK_DIR = Path("data/chunks")
+
+
+producer = KafkaProducer(
+    bootstrap_servers=KAFKA_SERVER,
+    value_serializer=lambda v: json.dumps(v).encode("utf-8")
+)
+
+
+chunks = sorted(CHUNK_DIR.glob("chunk_*.mp4"))
+
+for chunk_id, chunk_path in enumerate(chunks):
+
+    with open(chunk_path, "rb") as f:
+        chunk_data = f.read()
+
+    message = {
+        "video_id": "test_video",
+        "chunk_id": chunk_id,
+        "filename": chunk_path.name,
+        "data": chunk_data.hex()
+    }
+
+    producer.send(TOPIC, value=message)
+
+    print(f"Sent: {chunk_path.name}")
+
+producer.flush()
+producer.close()
+
+print("All chunks sent successfully.")
